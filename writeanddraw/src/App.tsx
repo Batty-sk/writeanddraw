b055755573d89f95a9e2e770dec24b22fe986773
Batty-sk <saorav.skumar@gmail.com>
2025-02-24 17:15:35 +0530

    add: scalabble folder structure

---
+++ writeanddraw/src/App.tsx
@@ -0,0 +1,12 @@
+
+import '@fontsource/poppins/700.css';   
+import Homepage from "./pages/Homepage"
+const App = () => {
+  return (
+    <div>
+<Homepage />
+    </div>
+  )
+}
+
+export default App
---
+++ writeanddraw/src/components/Banner.tsx
@@ -0,0 +1,10 @@
+
+const Banner = () => {
+  return (
+    <div className='bg-gradient-to-r from-green-400 to-blue-500 text-transparent bg-clip-text min-h-[50vh] flex items-center justify-center'>
+    <h1 className="font-poppins">Welcome To Write and Draw</h1>
+    </div>
+  )
+}
+
+export default Banner
